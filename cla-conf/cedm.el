bd9fb52bdd2b2664f5b2c8aa4a770e46709baa41
Christoph Landgraf <christoph.landgraf@contact-software.com>
2026-06-11 09:26:12 +0200

    CEDM Hydra

--- cla-conf/cedm.el
+++ cla-conf/cedm.el
@@ -16,28 +16,39 @@
 
 ;;; Code:
 
-(defconst cedm--md-insert-cr-command
-  "ceget cdberror %s | jq -r '.[] | [.[\"system:description\"], .[\"system:ui_link\"]] | \"[\\(.[0])](\\(.[1]))\"'")
+(defconst cedm--insert-detail-link-command
+  "ceget %s %%s | jq -r '.[] | [.[\"system:description\"], .[\"system:ui_link\"]] | %s'")
 
-(defun cedm-md-insert-change-request (cr)
-  (interactive
-   (list (read-string "Change Request ID: ")))
-  (if (not (string-match-p "^E[0-9]\\{6\\}$" cr))
-      (error "'%s' is no valid Change Reques  ID."))
+(defconst cedm--md-link-pattern
+  "\"[\\(.[0])](\\(.[1]))\"")
+
+(defconst cedm--org-link-pattern
+  "\"[[\\(.[1])][\\(.[0])]]\"")
+
+(defun cedm--link-pattern ()
+  (cond ((derived-mode-p 'org-mode)
+         cedm--org-link-pattern)
+        ((derived-mode-p 'markdown-mode)
+         cedm--md-link-pattern)
+        (t
+         (error "Unsupported mode"))))
+
+(defun cedm--insert-detail-link (object-class keys)
   (insert
    (string-trim-right
     (shell-command-to-string
-     (format cedm--md-insert-cr-command cr)))))
+     (format (format cedm--insert-detail-link-command object-class (cedm--link-pattern)) keys)))))
 
-(defconst cedm--md-insert-person-command
-  "ceget person %s | jq -r '.[0][\"system:description\"]'")
+(defun cedm-insert-change-request (cr)
+  (interactive
+   (list (read-string "Change Request ID: ")))
+  (if (not (string-match-p "^E[0-9]\\{6\\}$" cr))
+      (error "'%s' is no valid Change Request ID."))
+  (cedm--insert-detail-link "cdberror" cr))
 
 (defun cedm-md-insert-person (person)
   (interactive
    (list (read-string "Person ID: ")))
-  (insert
-   (string-trim-right
-    (shell-command-to-string
-     (format cedm--md-insert-person-command person)))))
+  (cedm--insert-detail-link "person" person))
 
 (provide 'cedm)
